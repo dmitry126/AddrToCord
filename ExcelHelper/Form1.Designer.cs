4168286313b1315a5cc3e3d016a56b6d5aba82b9
dmitry126 <100125125+dmitry126@users.noreply.github.com>
2024-03-04 16:33:06 +0300

    First Version

---
+++ ExcelHelper/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ExcelHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.btn_Calc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Path
+            // 
+            this.tb_Path.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tb_Path.Location = new System.Drawing.Point(12, 49);
+            this.tb_Path.Multiline = true;
+            this.tb_Path.Name = "tb_Path";
+            this.tb_Path.ReadOnly = true;
+            this.tb_Path.Size = new System.Drawing.Size(605, 45);
+            this.tb_Path.TabIndex = 0;
+            this.tb_Path.Text = "C:\\Users\\KEEP OUT NIGGER!!!\\Downloads\\Объекты.xlsx";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path:";
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_OpenFile.Location = new System.Drawing.Point(638, 49);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(114, 45);
+            this.btn_OpenFile.TabIndex = 2;
+            this.btn_OpenFile.Text = "...";
+            this.btn_OpenFile.UseVisualStyleBackColor = false;
+            this.btn_OpenFile.Click += new System.EventHandler(this.btn_OpenFile_Click);
+            // 
+            // btn_Calc
+            // 
+            this.btn_Calc.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_Calc.Location = new System.Drawing.Point(12, 109);
+            this.btn_Calc.Name = "btn_Calc";
+            this.btn_Calc.Size = new System.Drawing.Size(197, 45);
+            this.btn_Calc.TabIndex = 3;
+            this.btn_Calc.Text = "Calculate";
+            this.btn_Calc.UseVisualStyleBackColor = false;
+            this.btn_Calc.Click += new System.EventHandler(this.btn_Calc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 569);
+            this.Controls.Add(this.btn_Calc);
+            this.Controls.Add(this.btn_OpenFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Path);
+            this.Name = "Form1";
+            this.Text = "ExcelHelper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.Button btn_Calc;
+    }
+}
+
---
+++ ExcelHelper/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Xml;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace ExcelHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filePath;
+        string URL = "https://geocode-maps.yandex.ru/1.x/?apikey=4f98a5df-1522-46aa-8279-2a8691188448&geocode=";
+        private void btn_OpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "excel files (*.xlsx)|*.xlsx";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    tb_Path.Text = filePath;
+                }
+            }
+
+        }
+
+        private void btn_Calc_Click(object sender, EventArgs e)
+        {
+            if (tb_Path.Text != null) 
+            {
+                Excel.Application ObjExcel = new Excel.Application();
+                //Открываем книгу.                                                                                                                                                        
+                Workbook ObjWorkBook = ObjExcel.Workbooks.Open(tb_Path.Text, 0, false, 5, "", "", false, XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+                //Выбираем таблицу(лист).
+                Worksheet ObjWorkSheet;
+                ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[1];
+
+                ObjExcel.Visible = true;
+
+                Range last = ObjWorkSheet.Cells.SpecialCells(XlCellType.xlCellTypeLastCell, Type.Missing);
+                Range range = ObjWorkSheet.get_Range("A2", last);
+
+                //Iterate the rows in the used range
+                foreach (Excel.Range row in range.Rows)
+                { 
+                    string result = GetCord(URL + row.Columns[2].Value2 + ", " + row.Columns[5].Value2);
+                    string[] values = result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    row.Columns[17].Value2 = values[0];
+                    row.Columns[18].Value2 = values[1];
+
+                }
+            }
+        }
+        private string GetCord(string Http)
+        {
+            string l = "<Point xmlns=\"http://www.opengis.net/gml\"><pos>";
+            WebRequest req = WebRequest.Create(@"" + Http);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string text = reader.ReadToEnd();
+            text = text.Substring(text.IndexOf(l) + l.Length);
+            text = text.Remove(text.IndexOf("</pos>"));
+            return text;
+        }
+
+        private string GetCord2(string Http)
+        {
+            WebRequest req = WebRequest.Create(@"" + Http);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+
+            XmlDocument xDoc = new XmlDocument();
+
+            string xml = reader.ReadToEnd();
+
+            xDoc.LoadXml(xml);
+
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNode pos = xRoot.SelectSingleNode("//pos");
+
+
+            return pos.InnerXml;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = GetCord2(@"https://geocode-maps.yandex.ru/1.x/?apikey=4f98a5df-1522-46aa-8279-2a8691188448&geocode=Кобулети,%20просп.%20Давида%20Агмашенебели,%20291");
+        }
+    }
+}
